61141784e8f9a08225b9d226cb09a40d29b40b98
KraMD <maxkripe333@gmail.com>
2015-07-15 15:21:53 +0500

    1,4

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            Console.Write("Введите выражение");
+            Console.WriteLine();
+            Console.WriteLine();
+            string s = Console.ReadLine();
+            s = s.Remove(s.Length - 1);
+            string[] h = s.Split(new char[]{'+','-'});
+            for (int i = 0; i < h.Length; i++)
+                sum += Convert.ToInt32(h[i]);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Переписываем выражение с учётом его решения");
+            Console.WriteLine();
+            Console.Write("{0}={1}", s, sum);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Ответ: {0}", sum);
+            Console.ReadKey();
+        }
+    }
+}
